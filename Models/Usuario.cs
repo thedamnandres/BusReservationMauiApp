429b7287d9131a013d039a517c3986068d8cb36c
Andres Jimenez <andres_jimenez666@hotmail.com>
2024-12-10 07:34:55 -0500

    Modelo Usuario

---
+++ Models/Usuario.cs
@@ -0,0 +1,9 @@
+namespace BusReservationMauiApp.Models;
+
+public class Usuario
+{
+    public string Nombre { get; set; }
+    public string CI { get; set; }
+    public string Correo { get; set; }
+    public string Telefono { get; set; }
+}
--- Views/Usuario/UsuarioPage.xaml.cs
+++ Views/Usuario/UsuarioPage.xaml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using BusReservationMauiApp.Models;
 
 namespace BusReservationMauiApp.Views;
 
@@ -11,5 +12,17 @@ public partial class UsuarioPage : ContentPage
     public UsuarioPage()
     {
         InitializeComponent();
+
+        // Datos quemados
+        var usuario = new Usuario
+        {
+            Nombre = "Paul Larrea",
+            CI = "1765467382",
+            Correo = "paul.larrea@hotmail.com",
+            Telefono = "99 897 7653"
+        };
+
+        // Asignar el modelo como contexto de la vista
+        BindingContext = usuario;
     }
 }
